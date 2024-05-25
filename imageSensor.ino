bf2bc55118f89f5ddfc5091394f6cdafee7b9cc8
Dohyeon Kim <xingchen8837@gmail.com>
2024-05-25 16:02:56 +0900

    3차 실험

--- imageSensor.ino
+++ imageSensor.ino
@@ -26,47 +26,98 @@ public:
     void resetCap(int resetTime)
     {
         digitalWrite(resetPin, HIGH);
-        Serial.print("Resetting... resetTime: ");
-        Serial.println(resetTime);
+        if (interruptTriggered)
+        {
+            Serial.print("Resetting... resetTime: ");
+            Serial.println(resetTime);
+        }
         delay(resetTime);
         digitalWrite(resetPin, LOW);
-        Serial.println("Reset complete.");
+        if (interruptTriggered)
+        {
+            Serial.println("Reset complete.");
+        }
     }
 
     float readLevel(int resetTime, int expTime)
     {
         this->resetCap(resetTime);
-        Serial.print("Exposing... expTime: ");
-        Serial.println(expTime);
+        if (interruptTriggered)
+        {
+            Serial.print("Exposing... expTime: ");
+            Serial.println(expTime);
+        }
         delay(expTime);
-        Serial.println("Expose complete.");
+        if (interruptTriggered)
+        {
+            Serial.println("Expose complete.");
+        }
         int level = analogRead(levelPin);
-        Serial.println("Signal level recorded.");
         return ((4096 - level) / 4096.0) * 3.3;
     }
 };
 
-void
-setup()
+void setup()
 {
     Serial.begin(115200);
     pinMode(RESET, OUTPUT);
     pinMode(BTN, INPUT_PULLUP);
     pinMode(COLUMN, INPUT);
     attachInterrupt(digitalPinToInterrupt(BTN), handleInterrupt, FALLING);
-    Serial.println("initialized!");
 }
+
+
+Pixel P = Pixel(RESET, COLUMN);
+int resetTime = 3000, expTime = 5000;
 
 void loop()
 {
     if (interruptTriggered)
     {
-        Pixel P = Pixel(RESET, COLUMN);
-        float level = P.readLevel(3000, 5000);
+        float level = P.readLevel(resetTime, expTime);
         Serial.println(level);
 
         interruptTriggered = false; // 인터럽트 처리 완료 표시
         delay(100);
         attachInterrupt(digitalPinToInterrupt(BTN), handleInterrupt, FALLING); // 인터럽트 다시 활성화
+    }
+
+    if (Serial.available())
+    {
+        String input = Serial.readStringUntil('\n');
+        input.trim(); // 입력 문자열의 앞뒤 공백 제거
+
+        if (input.startsWith("r"))
+        {
+            Serial.println(resetTime + expTime);
+            float level = P.readLevel(resetTime, expTime);
+            Serial.println(level);
+        }
+        else if (input.startsWith("c "))
+        {
+            input.remove(0, 2);
+            int firstSpace = input.indexOf(' ');
+            if (firstSpace == -1)
+            {
+                Serial.println("Invalid command format");
+                return;
+            }
+
+            String resetTimeStr = input.substring(0, firstSpace);
+            String expTimeStr = input.substring(firstSpace + 1);
+
+            // 문자열을 숫자로 변환
+            resetTime = resetTimeStr.toInt();
+            expTime = expTimeStr.toInt();
+            Serial.printf("Changed! resetTime: %d, expTime: %d\n", resetTime, expTime);
+        }
+        else if (input.startsWith("p")) 
+        {
+            Serial.printf("Printing configs... resetTime: %d, expTime: %d\n", resetTime, expTime);
+        }
+        else 
+        {
+            Serial.println("Invalid command");
+        }
     }
 }
